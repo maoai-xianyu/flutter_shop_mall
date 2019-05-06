ea7e04b17c69788016ac28a627394293a5f6b767
zhangkun@boxfish.cn <194264514@qq.com>
2019-05-06 18:30:19 +0800

    显示底部标签

--- README.md
+++ README.md
@@ -51,3 +51,41 @@ class MyApp extends StatelessWidget {
 1. Flutter两种UI风格的区别和如何一起使用。
 2. Flutter动态小部件的基本使用-StatefulWidget。
 3. 声明一个BottomNavigationBarItem类型的List，并设置文字和图标
+
+
+# 第04节：打通底部导航栏
+
+> 在pages目录下，我们新建下面四个dart文件。
+
+1. home_page.dart :商城首页UI页面，首页相关的UI我们都会放到这个文件里。
+2. category_page.dart: 商城分类UI页面，这个页面会有复杂的动态组件切换。
+3. cart_page.dart：商城购物车UI页面，这个页面会包括购物车的全套功能。
+4. member_page.dart：商城会员中心页面，这个页面我们会制作会员中心的全部UI效果。
+
+* 声明两个变量，并进行initState初始化：
+
+1. currentIndex： int类型，负责tabBodies的List索引，改变索引就相当于改变了页面。
+2. currentPage: 利用currentIndex得到当前选择的页面，并进行呈现出来。
+
+```
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
+      bottomNavigationBar: BottomNavigationBar(
+        type:BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        items:bottomTabs,
+        onTap: (index){
+          setState(() {
+           currentIndex = index;
+           currentPage = tabBodies[currentIndex]; 
+          });
+        },
+      ),
+      body:currentPage
+    );
+  }
+```
+
+> type:BottomNavigationBarType.fixed,这个是设置底部tab的样式，它有两种样式fixed和shifting
--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'home_page.dart';
+import 'category_page.dart';
+import 'cart_page.dart';
+import 'member_page.dart';
 
 class IndexPage extends StatefulWidget {
   @override
@@ -7,7 +11,6 @@ class IndexPage extends StatefulWidget {
 }
 
 class _IndexPageState extends State<IndexPage> {
-
   final List<BottomNavigationBarItem> bottomBars = [
     BottomNavigationBarItem(
       icon: Icon(CupertinoIcons.home),
@@ -17,7 +20,6 @@ class _IndexPageState extends State<IndexPage> {
       icon: Icon(CupertinoIcons.search),
       title: Text('分类'),
     ),
-
     BottomNavigationBarItem(
       icon: Icon(CupertinoIcons.shopping_cart),
       title: Text('购物车'),
@@ -27,9 +29,39 @@ class _IndexPageState extends State<IndexPage> {
       title: Text('首页'),
     ),
   ];
+
+  final List tabBodies = [
+    HomePage(),
+    CategoryPage(),
+    CartPage(),
+    MemberPage(),
+  ];
+
+  int _currentIndex = 0;
+  var _currentPage;
+
+  @override
+  void initState() {
+    _currentPage = tabBodies[_currentIndex];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
+      body: _currentPage,
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: bottomBars,
+        currentIndex: _currentIndex,
+        onTap: (index){
+          setState(() {
+            _currentIndex = index;
+            _currentPage = tabBodies[_currentIndex];
+          });
+        },
+      ),
+    );
   }
 }
-
