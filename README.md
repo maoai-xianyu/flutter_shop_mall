11623c3bea4a4eb9341696166ba48e82e41eb8fb
zhangkun@boxfish.cn <194264514@qq.com>
2019-05-06 17:29:25 +0800

    一部分

--- README.md
+++ README.md
@@ -35,3 +35,19 @@ class MyApp extends StatelessWidget {
 }
 
 ```
+
+## 第03节: 底部导航栏制作
+
+* cupertino_IOS风格介绍
+
+在Flutter里是有两种内置风格的：
+
+1. material.dart风格： Material Design 是由Google推出的全新设计语言，这种设计语言是为手机、平板电脑、台式机和其他平台提供一致，更广泛的外观和感觉。我喜欢称它为纸墨设计。Material Design 风格是一种非常有质感的设计风格，并会提供一些默认的交互动画。
+2. cupertino.dart风格：即IOS风格组件，它重现了很多经典的有IOS特性的交互和界面风格，让适用于IOS的人感觉亲切和友好。
+
+
+* 使用动态StatefulWidget
+
+1. Flutter两种UI风格的区别和如何一起使用。
+2. Flutter动态小部件的基本使用-StatefulWidget。
+3. 声明一个BottomNavigationBarItem类型的List，并设置文字和图标
--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-class IndexPage extends StatelessWidget {
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+
+  final List<BottomNavigationBarItem> bottomBars = [
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text('首页'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.search),
+      title: Text('分类'),
+    ),
+
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.shopping_cart),
+      title: Text('购物车'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.profile_circled),
+      title: Text('首页'),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-      appBar: AppBar(
-        title: Text('百姓生活+'),
-      ),
-      body: Center(
-        child: Text('百姓生活+'),
-      ),
-    );
+    return Container();
   }
 }
+
