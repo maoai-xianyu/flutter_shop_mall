200d2d643cb5c9acd14321dec983243e0c47505a
zhangkun@boxfish.cn <194264514@qq.com>
2020-01-06 19:12:44 +0800

    更新

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/zhangkun/zk_develop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/zhangkun/workspace/flutter_shop_mall"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/zhangkun/zk_develop/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
